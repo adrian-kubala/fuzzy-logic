c780e5e905a5dcfa86b15838a02da485c6afaefe
adrian <adrian@macbook-pro-adrian.home>
2016-12-12 19:54:51 +0100

    Add working chart

---
+++ src/pl/fuzzy/TrapezeTerm.java
@@ -0,0 +1,14 @@
+package pl.fuzzy;
+
+/**
+ * Created by adrian on 04.12.2016.
+ */
+public class TrapezeTerm extends TriangularTerm {
+    double x1;
+
+    public TrapezeTerm(String name, double a, double x0, double x1, double b) {
+        super(name, a, x0, b);
+
+        this.x1 = x1;
+    }
+}
