220900fc093ed23520bb04b3576553b78c79a5a3
adrian <adrian@macbook-pro-adrian.home>
2016-12-14 02:11:25 +0100

    Add fuzzify method to FuzzySet class

--- src/modell/FuzzySet.java
+++ src/modell/FuzzySet.java
@@ -11,6 +11,7 @@ public class FuzzySet extends XYSeriesCollection {
 
     public String name;
     public Range range;
+    double membershipValues[];
 
     public FuzzySet(String name) {
         super();
@@ -20,5 +21,28 @@ public class FuzzySet extends XYSeriesCollection {
 
     public XYDataset getDataSet() {
         return (XYDataset) this;
+    }
+    
+    public void fuzzify(double x) {
+        Term<Temperature>[] tempTerms = (Term<Temperature>[]) getSeries().toArray();
+        
+        membershipValues = new double[tempTerms.length];
+        for (int i = 0; i < tempTerms.length; i++) {
+            
+            double a = tempTerms[i].a;
+            double x0 = tempTerms[i].x0;
+            double x1 = tempTerms[i].x1;
+            double b = tempTerms[i].b;
+
+            if (x >= a && x <= x0) {
+                membershipValues[i] = (x - a) / (x0 - a);
+            } else if (x >= x0 && x <= x1) {
+                membershipValues[i] = 1;
+            } else if (x >= x1 && x <= b) {
+                membershipValues[i] = (b - x) / (b - x1);
+            } else {
+                membershipValues[i] = 0;
+            }
+        }
     }
 }
--- src/modell/Term.java
+++ src/modell/Term.java
@@ -9,6 +9,8 @@ import org.jfree.data.xy.XYSeries;
 public class Term <E extends Enum<E>> extends XYSeries {
 
     private final Enum<E> type;
+    double a, x0, x1, b;
+    
 
     public Term(String name, Enum<E> type) {
         super(name);
@@ -18,6 +20,11 @@ public class Term <E extends Enum<E>> extends XYSeries {
     }
 
     public void setShape(double a, double x0, double x1, double b) {
+        this.a = a;
+        this.x0 = x0;
+        this.x1 = x1;
+        this.b = b;
+        
         add(a, 0);
         add(x0, 1);
         addOrUpdate(x1, 1);
