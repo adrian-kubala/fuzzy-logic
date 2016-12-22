aeca58765d5ac687cc2107593c753273ea8d4b30
adrian <adrian@macbook-pro-adrian.home>
2016-12-22 20:02:17 +0100

    Create singleton class

---
+++ src/other/NumbersFormatter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package other;
+
+/**
+ *
+ * @author adrian
+ */
+public class NumbersFormatter {
+    public static final NumbersFormatter instance = new NumbersFormatter();
+    
+    private NumbersFormatter() { };
+    
+    public double roundToDecimalPlaces(double value, int places) {
+        double factor = Math.pow(10, places);
+        return Math.round(value * factor) / factor;
+    }
+}
