c6ebc92a3188bc60b7b6f08e83d9351e3c694b40
adrian <adrian@macbook-pro-adrian.home>
2016-12-28 15:19:38 +0100

    Remove appropriate view

--- src/other/MainFrame.java
+++ src/other/MainFrame.java
@@ -172,10 +172,9 @@ public class MainFrame extends javax.swing.JFrame {
             fuzzyOutputTextArea.append("u" + name + " (" + boilerTemperature.name + ") = " + value + "\n");
         }
         
-        inferenceBlockPanel.remove(1);
+        inferenceBlockPanel.remove(controller.inferenceBlockView);
         controller.infer();
-        
-        inferenceBlockPanel.add(controller.inferenceBlockView);
+        inferenceBlockPanel.add(controller.inferenceBlockView, 1);
         inferenceBlockPanel.revalidate();
         inferenceBlockPanel.repaint();        
     }//GEN-LAST:event_fuzzifyButtonActionPerformed
