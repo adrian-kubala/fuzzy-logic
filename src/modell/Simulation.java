06e9647aa5fe9b653320860d16a826e79a11a7c4
adrian <adrian@macbook-pro-adrian.home>
2017-01-07 12:42:08 +0100

    Add initTimer method

--- src/modell/Simulation.java
+++ src/modell/Simulation.java
@@ -17,34 +17,38 @@ import other.SimulationDelegate;
  * @author adrian
  */
 public class Simulation extends TimerTask {
-    
+
     OutsideTemperature outsideTemperature;
     Boiler boiler;
-    
+
     Timer timer = new Timer();
     public SimulationDelegate delegate;
-    
+
     public Simulation() {
         initOutsideTemperature();
         initBoiler();
+        initTimer();
+    }
+
+    private void initTimer() {
         timer.schedule(this, 0, 1000);
     }
-    
+
     private void initOutsideTemperature() {
         outsideTemperature = new OutsideTemperature(new Range(-30, 5));
     }
-    
+
     private void initBoiler() {
         boiler = new Boiler(new Range(35, 75));
         boiler.specifyDesiredTemperatureBasedOn(outsideTemperature);
     }
-    
+
     @Override
     public void run() {
         if (boiler.didReachDesiredTemperature()) {
             return;
         }
-        
+
         double outputValue = delegate.inputValueDidChange(boiler.temperature);
         boiler.temperature += (Math.abs(outsideTemperature.range.getLength()) + boiler.desiredTemperature) / 100 * outputValue;
     }
